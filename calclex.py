c4013b4075b61347761913106192202304dfa068
icecraft <tmortred@gmail.com>
2016-04-27 23:36:32 +0800

    增加 bison，准备编写bison 规则

--- calclex.py
+++ calclex.py
@@ -9,19 +9,47 @@ reserved = {
     'then': 'THEN',
     'when': 'WHEN',
     'define': 'DEFINE',
-    'end': 'END',
-    'eq': 'EQ',
-    'neq': 'NEQ',
     'print': 'PRINT'
     }
+
 
 # List of token names.   This is always required
 tokens = [
     'NUMBER',
-    'NAME'
+    'NAME',
+    'EOL',
+    'LT',
+    'GT',
+    'EQ',
+    'NEQ',
+    'CMP'
 ] + list(reserved.values())
 
-literals = ['+', '-', '/', '*', '(', ')', '<', '>', '=']
+literals = ['+', '-', '/', '*', '(', ')', ',', '=', ';']
+
+
+def t_GT(t):
+    r'\>'
+    t.type = 'CMP'
+    return t
+
+
+def t_LT(t):
+    r'\<'
+    t.type = 'CMP'
+    return t
+
+
+def t_EQ(t):
+    r'=='
+    t.type = 'CMP'
+    return t
+
+
+def t_NEQ(t):
+    r'!='
+    t.type = 'CMP'
+    return t
 
 
 # A regular expression rule with some action code
@@ -37,11 +65,12 @@ def t_NAME(t):
     return t
 
 
-# Define a rule so we can track line numbers
-def t_newline(t):
+def t_EOF(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-    
+    return t
+
+
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ' \t'
 
@@ -55,18 +84,76 @@ def t_error(t):
 lexer = lex.lex()
 
 data = '''
-define a(arg1, arg2)
+define a( arg1, arg2 )
    b = 2
-   b < 3
+   b != 3
 end
 '''
 
 # Give the lexer some input
 lexer.input(data)
 
+
+precedence = (
+    ('nonassoc', 'CMP'),
+    ('right', '='),
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('nonassoc', 'UMINUS'),
+    )
+
 # Tokenize
 while True:
     tok = lexer.token()
     if not tok: 
         break      # No more input
-    print(tok)
+    print (tok)
+
+    
+"""
+# bison rules
+
+stmt: IF exp THEN list            
+   | IF exp THEN list ELSE list  
+   | WHEN exp THEN list           
+   | exp
+;
+
+list: /* nothing */ { $$ = NULL; }
+   | stmt ';' list  {XX}                    }
+   ;
+
+exp: exp CMP exp          { $$ = newcmp($2, $1, $3); }
+   | exp '+' exp          { $$ = newast('+', $1,$3); }
+   | exp '-' exp          { $$ = newast('-', $1,$3);}
+   | exp '*' exp          { $$ = newast('*', $1,$3); }
+   | exp '/' exp          { $$ = newast('/', $1,$3); }
+   | '(' exp ')'          { $$ = $2; }
+   | '-' exp %prec UMINUS { $$ = newast('M', $2, NULL); }
+   | NUMBER               { $$ = newnum($1); }
+   | NAME                 { $$ = newref($1); }
+   | NAME '=' exp         { $$ = newasgn($1, $3); }
+   | NAME '(' explist ')' { $$ = newcall($1, $3); }
+;
+
+explist: exp
+ | exp ',' explist  { $$ = newast('L', $1, $3); }
+;
+symlist: NAME       { $$ = newsymlist($1, NULL); }
+ | NAME ',' symlist { $$ = newsymlist($1, $3); }
+;
+
+calclist: /* nothing */
+  | calclist stmt EOL {
+    if(debug) dumpast($2, 0);
+     printf("= %4.4g\n> ", eval($2));
+     treefree($2);
+    }
+  | calclist LET NAME '(' symlist ')' '=' list EOL {
+                       dodef($3, $5, $8);
+                       printf("Defined %s\n> ", $3->name); }
+
+  | calclist error EOL { yyerrok; printf("> "); }
+ ;
+
+"""
